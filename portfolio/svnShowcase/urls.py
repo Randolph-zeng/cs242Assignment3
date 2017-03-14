0667d12cb510bb1a520569886cfda509c80c30d2
Ruiss_9 <randolphzeng@gmail.com>
2017-03-14 12:29:22 -0500

    files template

--- portfolio/svnShowcase/urls.py
+++ portfolio/svnShowcase/urls.py
@@ -5,4 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^portfolio/$', views.getPortfolio),
 		url(r'^home/$', TemplateView.as_view(template_name="index.html")),
+		url(r'^files/$', TemplateView.as_view(template_name="files.html")),
 ]
