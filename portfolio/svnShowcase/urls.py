1524d271a5b73237a082f7332f58a98c7f3683a7
Ruiss_9 <randolphzeng@gmail.com>
2017-03-14 11:03:40 -0500

    try to add static page

--- portfolio/svnShowcase/urls.py
+++ portfolio/svnShowcase/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from svnShowcase import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^portfolio/$', views.getPortfolio),
+		url(r'^home/$', TemplateView.as_view(template_name="index.html")),
 ]
